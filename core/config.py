36b3b0622d6fad86b2c1eb0ad7f0f7c6bca5f2a8
MihailRytov <rtvmhl@gmail.com>
2024-05-18 16:08:20 +0300

    __init__.py database update db_helper.py

--- core/config.py
+++ core/config.py
@@ -5,6 +5,7 @@ DATABASE_URL: str = "postgresql+asyncpg://admin:admin@localhost/servicebase"
 
 class Setting(BaseSettings):
     db_url: str = DATABASE_URL
+    db_echo: bool = True
 
 
 settings = Setting()
--- core/database/__init__.py
+++ core/database/__init__.py
@@ -1,7 +1,12 @@
 __all__ = (
     "Base",
     "Brand",
+    "Product",
+    "Service_Center",
+    "Client",
+    "Document",
+    "Provider",
 )
 
 from .base import Base
-from .models import Brand
+from .models import Brand, Product, Service_Center, Client, Document, Provider
